b838dd37fb055233b69972825d9be9e3804cca27
ishi-taku034 <ishimaru0645@icloud.com>
2022-04-26 04:48:38 +0900

    shieldオブジェクトの追加とCube複製時に起きているエラー

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,6 +9,19 @@ using UnityEngine.UI;
 
 public class Bullet : MonoBehaviour 
 {
-    
+    public GameObject _bullets;
 
+    public void Start()
+    {
+        for (int x = 0; x < _bullets.GetComponents().Length; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                for (int z = 0; z < length; z++)
+                {
+
+                }
+            }
+        }
+    }
 }
